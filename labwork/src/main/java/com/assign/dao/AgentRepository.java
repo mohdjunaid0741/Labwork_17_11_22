5bff1350b94b644d25c7c6020fa2bc379f38a0ca
mohdjunaid0741 <109589436+mohdjunaid0741@users.noreply.github.com>
2022-11-20 01:20:33 +0530

    Delete labwork directory

--- labwork/src/main/java/com/assign/dao/AgentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.assign.dao;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.assign.agent.Agent;
-
-public interface AgentRepository extends CrudRepository<Agent,Integer>{
-
-}
