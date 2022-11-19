32c66408bcce9d2a4e739362532b3780f2836a9a
mohdjunaid0741 <109589436+mohdjunaid0741@users.noreply.github.com>
2022-11-19 21:58:52 +0530

    Add files via upload

---
+++ labwork/src/main/java/com/assign/dao/AgentRepository.java
@@ -0,0 +1,9 @@
+package com.assign.dao;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.assign.agent.Agent;
+
+public interface AgentRepository extends CrudRepository<Agent,Integer>{
+
+}
